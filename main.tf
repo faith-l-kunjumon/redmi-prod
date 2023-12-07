d5b6688353507f507fa578dd42ec27bf9634558d
faith <faithkunjumon@gmail.com>
2023-12-07 13:30:48 +0000

    port 3306 configured

--- main.tf
+++ main.tf
@@ -64,6 +64,16 @@ resource "aws_security_group" "remote-access" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+ingress {
+    description = "SSH from VPC"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
   egress {
     from_port        = 0
     to_port          = 0
