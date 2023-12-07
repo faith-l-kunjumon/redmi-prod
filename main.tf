38a642053a917dfad93c44dba2f088c9fd548b0f
faith <faithkunjumon@gmail.com>
2023-12-07 13:25:45 +0000

    remote sg created

--- main.tf
+++ main.tf
@@ -47,3 +47,36 @@ resource "aws_security_group" "frontend" {
   }
 }
 
+
+#remote access
+
+resource "aws_security_group" "remote-access" {
+  name        = "${var.project_name}.${var.project_env}-remote-access"
+  description = "${var.project_name}.${var.project_env}-remote-access"
+
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name    = "${var.project_name}.${var.project_env}-remote-access"
+    Project = var.project_name
+    Env     = var.project_env
+  }
+}
+
+
